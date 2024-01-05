9ac72a6ac4d831240fbf91e3d8de9189d679bac4
Mike Farquhar <michaelfarquhar1991@gmail.com>
2024-01-05 22:32:41 +0000

    Initial commit

---
+++ lua/plugins/neo-tree.lua
@@ -0,0 +1,16 @@
+vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	branch = "v3.x",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function()
+		vim.keymap.set("n", "<C-n>", ":Neotree filesystem toggle left<CR>", {})
+		vim.keymap.set("n", "<leader>bf", ":Neotree buffers reveal float<CR>", {})
+		require("neo-tree").setup({})
+	end,
+}
